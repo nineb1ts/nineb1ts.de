ff19ef06dc8ed57635ca4e97d9553adc43b8f5bf
nineb1ts <jeskemal@gmail.com>
2026-07-18 12:24:08 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="page">
+      <main>
+        <section className="hero">
+          <div className="container grid">
+            <div className="hero-content">
+              <h1 className="hero-title">
+                nineb<span className="one">1</span>t<span className="s">s</span>
+              </h1>
+
+              <p className="hero-subtitle">&gt; sharpening claws...</p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
