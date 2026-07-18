1c6131e7b3bc4b6fc8b7e08b3b459b2faa1f4b6e
nineb1ts <jeskemal@gmail.com>
2026-07-18 22:01:40 +0200

    refactor: split section styles into separate files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import "./App.css";
+import "./styles/layout.css";
+import "./styles/hero.css";
+import "./styles/intro.css";
+import "./styles/journey.css";
+import "./styles/projects.css";
+import "./styles/contact.css";
+import "./styles/footer.css";
 import Hero from "./components/Hero";
 import IntroSection from "./components/IntroSection";
 import JourneySection from "./components/JourneySection";
